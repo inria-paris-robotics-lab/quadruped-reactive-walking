452122d43ff58f94c64976d11b0ff2181ff63767
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-10 15:33:33 +0100

    python/module.cpp : expose Eigen::Ref of const Vector6

--- python/module.cpp
+++ python/module.cpp
@@ -1,10 +1,13 @@
 #include "bindings/python.hpp"
+#include "qrw/Types.h"
 
 BOOST_PYTHON_MODULE(quadruped_reactive_walking_pywrap) {
   bp::docstring_options module_docstring_options(true, true, true);
   eigenpy::enableEigenPy();
 
   bp::import("warnings");
+
+  eigenpy::enableEigenPySpecific<Vector6>();
 
   exposeAnimators();
   exposeParams();
