056e38f5326efec9689c2c6106f35e7502e4d2e7
ManifoldFR <wilson.jallet@polytechnique.org>
2023-02-17 15:22:33 +0100

    add example script building OCP outside of Controller and wrapper class

---
+++ examples/simple_ocp.py
@@ -0,0 +1,41 @@
+import numpy as np
+import quadruped_reactive_walking as qrw
+
+from quadruped_reactive_walking.wb_mpc import AlgtrOCP, CrocOCP
+from quadruped_reactive_walking.controller import make_footsteps_and_refs
+from quadruped_reactive_walking.wb_mpc.problem_data import TaskSpec
+from quadruped_reactive_walking.wb_mpc.target import Target
+from quadruped_reactive_walking.tools.Utils import init_robot
+
+params = qrw.Params()
+params.ocp: qrw.OCPParams
+params.ocp.init_max_iters = 1
+q_init = np.array(params.q_init.tolist())
+init_robot(q_init, params)
+task = TaskSpec(params)
+
+print(task)
+
+target = Target(params)
+
+footsteps, base_refs = make_footsteps_and_refs(params, target)
+
+x0 = task.x0
+print("x0:", x0)
+
+ocp = CrocOCP(task, params, footsteps, base_refs)
+
+nsteps = ocp.ddp.problem.T
+xs_i = [x0] * (nsteps + 1)
+us_i = ocp.problem.quasiStatic(xs_i[:nsteps])
+
+ocp.solve(0, xs_i, us_i)
+
+
+ocp2 = AlgtrOCP(task, params, footsteps, base_refs)
+ocp2.solve(0, xs_i, us_i)
+ocp2_res = ocp2.prox_ddp.getResults()
+
+
+dist_x = np.linalg.norm(np.stack(ocp.ddp.xs) - np.stack(ocp2_res.xs))
+print("Dist X:", dist_x)
