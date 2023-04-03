12e4953f86bf5a57adfb3f0ed92f7c88d0dc348e
ManifoldFR <wilson.jallet@polytechnique.org>
2023-04-03 11:54:22 +0200

    replay_log.py: add video recording

--- replay_log.py
+++ replay_log.py
@@ -4,11 +4,12 @@ import quadruped_reactive_walking as qrw
 
 from pathlib import Path
 from pinocchio.visualize import MeshcatVisualizer
-from quadruped_reactive_walking.wb_mpc.task_spec import TaskSpecBase
 
-import meshcat
 import pprint
 import example_robot_data as erd
+import hppfcl
+import imageio
+import pinocchio as pin
 
 params = qrw.Params.create_from_file()
 
@@ -22,8 +23,10 @@ robot = erd.load("solo12")
 
 def parse_args():
     import argparse
+
     parser = argparse.ArgumentParser()
     parser.add_argument("logfile", type=Path)
+    parser.add_argument("--record", action="store_true")
     return parser.parse_args()
 
 
@@ -46,11 +49,7 @@ joints_ = data["q_mes"]
 q_rcn = np.concatenate([psn, orn, joints_], axis=1)
 q_rcn = q_rcn[::WBC_RATIO]
 
-
-import hppfcl
-import pinocchio as pin
-
-plane = hppfcl.Plane(np.array([0, 0, 1]), 0.)
+plane = hppfcl.Plane(np.array([0, 0, 1]), 0.0)
 geobj = pin.GeometryObject("plane", 0, pin.SE3.Identity(), plane)
 geobj.meshColor[:] = 0.6, 0.1, 0.1, 0.8
 geobj.meshScale *= 1.5
@@ -63,3 +62,16 @@ vizer = MeshcatVisualizer(rmodel, robot.collision_model, vmodel)
 vizer.initViewer(loadModel=True)
 vizer.setBackgroundColor()
 vizer.display(q_rcn[0])
+
+
+if args.record:
+    VID_FPS = 0.5 / dt
+    video = imageio.get_writer("replay.mp4", fps=VID_FPS)
+else:
+    video = None
+
+
+def callback(t):
+    img = vizer.captureImage()
+    if video:
+        video.append_data(img)
