bc6effa8e8a93f8bd7feec729bffa33fdbd3bf20
ManifoldFR <wilson.jallet@polytechnique.org>
2023-04-07 17:48:58 +0200

    data safety: forbid std::vector members of MPCResult from being resized

--- python/MPCResult.cpp
+++ python/MPCResult.cpp
@@ -2,6 +2,36 @@
 
 #include "bindings/python.hpp"
 
+namespace detail {
+
+/// Util class to raise error when stl container member is resized.
+template <typename Class, typename vector_type>
+struct non_resizable_vec_member {
+ private:
+  vector_type Class::*m_which;
+
+ public:
+  non_resizable_vec_member(vector_type Class::*which) : m_which(which) {}
+
+  void operator()(Class &c, const vector_type &rhs) {
+    auto &lhs = c.*m_which;
+    if (lhs.size() != rhs.size()) {
+      PyErr_SetString(PyExc_ValueError, "vector size is non-mutable.");
+      bp::throw_error_already_set();
+    }
+    lhs = rhs;
+  }
+};
+}  // namespace detail
+
+template <typename Class, typename vector_type>
+bp::object make_non_resizable_vec_member(vector_type Class::*which) {
+  return bp::make_function(
+      detail::non_resizable_vec_member<Class, vector_type>(which),
+      bp::default_call_policies(),
+      boost::mpl::vector3<void, Class &, const vector_type &>());
+}
+
 void exposeMPCResult() {
   bp::class_<MPCResult>(
       "MPCResult", "MPC result struct.",
@@ -9,8 +39,12 @@ void exposeMPCResult() {
           bp::args("self", "Ngait", "nx", "nu", "ndx", "window_size")))
       .def_readwrite("gait", &MPCResult::gait)
       .def_readwrite("xs", &MPCResult::xs)
-      .def_readwrite("us", &MPCResult::us)
-      .def_readwrite("K", &MPCResult::Ks)
+      .add_property("xs", bp::make_getter(&MPCResult::xs),
+                    make_non_resizable_vec_member(&MPCResult::xs))
+      .add_property("us", bp::make_getter(&MPCResult::us),
+                    make_non_resizable_vec_member(&MPCResult::us))
+      .add_property("K", bp::make_getter(&MPCResult::Ks),
+                    make_non_resizable_vec_member(&MPCResult::Ks))
       .def_readwrite("solving_duration", &MPCResult::solving_duration)
       .def_readwrite("num_iters", &MPCResult::num_iters)
       .def_readwrite("new_result", &MPCResult::new_result);
