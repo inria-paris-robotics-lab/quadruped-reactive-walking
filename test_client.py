7f3e1930a4fb85e543a3bd1106addb177b381735
Wilson Jallet <wilson.jallet@inria.fr>
2023-04-17 18:16:57 +0200

    commit test_client.py

---
+++ test_client.py
@@ -0,0 +1,34 @@
+import rospy
+from quadruped_reactive_walking.srv import (
+    MPCSolve,
+    MPCSolveRequest,
+    MPCSolveResponse,
+)
+
+
+class TestClient():
+    """
+    Wrapper to run both types of MPC (OQSP or Crocoddyl) on a seperate node/machine using ROS as communication interface.
+    """
+
+    def __init__(
+            self,
+        ):
+        self.solve_solver_srv = rospy.ServiceProxy("qrw_wbmpc/test", MPCSolve, persistent=True)
+
+    def solve(self):
+        res = self.solve_solver_srv(MPCSolveRequest())
+
+class TestServer:
+    def __init__(self):
+        self._solve_service = rospy.Service(
+            "qrw_wbmpc/test", MPCSolve, self._trigger_solve
+        )
+
+    def _trigger_solve(self, msg):
+        return MPCSolveResponse(run_success=False)
+
+if __name__ == "__main__":
+    rospy.init_node("qrw_wbmpc")
+    server = TestServer()
+    rospy.spin()
