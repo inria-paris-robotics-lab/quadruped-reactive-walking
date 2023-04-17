90f1d1224c52af0a2c574b51e8273d2805001ebb
Wilson Jallet <wilson.jallet@inria.fr>
2023-04-17 19:26:20 +0200

    test_client: add some fake data

--- test_client.py
+++ test_client.py
@@ -19,7 +19,7 @@ class TestClient:
         )
 
     def solve(self):
-        self.solve_solver_srv(MPCSolveRequest())
+        return self.solve_solver_srv(MPCSolveRequest())
 
 
 class TestServer:
@@ -29,7 +29,24 @@ class TestServer:
         )
 
     def _trigger_solve(self, msg):
-        return MPCSolveResponse(run_success=False)
+        return MPCSolveResponse(run_success=True)
+
+
+def fake_data():
+    import numpy as np
+    from quadruped_reactive_walking.tools.ros_tools import numpy_to_multiarray_float64
+
+    k = 0
+    x0 = np.random.randn(37)
+    footstep = np.random.randn(3, 4)
+    base_ref = np.array([0.5, 0, 0, 0, 0, 0])
+    req = MPCSolveRequest(
+        k=k,
+        x0=numpy_to_multiarray_float64(x0),
+        footstep=numpy_to_multiarray_float64(footstep),
+        base_ref=numpy_to_multiarray_float64(base_ref),
+    )
+    return req
 
 
 if __name__ == "__main__":
