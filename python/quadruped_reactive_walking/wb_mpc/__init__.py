c93d93c33ebd4b5dd12246080ed15603ed8e51d0
ManifoldFR <wilson.jallet@polytechnique.org>
2023-05-17 15:51:53 +0200

    wb_mpc subpackage: define __all__ in __init__ file

--- python/quadruped_reactive_walking/wb_mpc/__init__.py
+++ python/quadruped_reactive_walking/wb_mpc/__init__.py
@@ -1,16 +1,22 @@
 from .ocp_abstract import OCPAbstract
 from .ocp_crocoddyl import CrocOCP
-from .ocp_proxddp import AlgtrOCPAbstract, AlgtrOCPProx, AlgtrOCPFDDP
+from .ocp_proxddp import AlgtrOCPProx, AlgtrOCPFDDP
 
-OCP_TYPES = (CrocOCP, AlgtrOCPProx, AlgtrOCPFDDP)
+_OCP_TYPES = (CrocOCP, AlgtrOCPProx, AlgtrOCPFDDP)
 
 
 def get_ocp_from_str(type_str):
-    for ocp in OCP_TYPES:
+    for ocp in _OCP_TYPES:
         if ocp.get_type_str() == type_str:
             return ocp
     raise ValueError("No OCP class named: " + type_str)
 
 
 def get_ocp_list_str():
-    return [ocp.get_type_str() for ocp in OCP_TYPES]
+    return [ocp.get_type_str() for ocp in _OCP_TYPES]
+
+
+OCP_TYPE_MAP = {ocp_cls.get_type_str(): ocp_cls for ocp_cls in _OCP_TYPES}
+
+
+__all__ = ["OCPAbstract", "CrocOCP", "AlgtrOCPProx", "AlgtrOCPFDDP", "OCP_TYPE_MAP"]
