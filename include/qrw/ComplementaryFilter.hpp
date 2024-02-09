e02023e630255fae13f16b68ce1b5c5ce018385a
ManifoldFR <wilson.jallet@polytechnique.org>
2024-02-09 16:23:07 +0100

    ComplementarityFilter.hpp : fix formatting
    ComplementarityFilter.hpp : fix const-correctness

--- include/qrw/ComplementaryFilter.hpp
+++ include/qrw/ComplementaryFilter.hpp
@@ -14,65 +14,46 @@
 
 class ComplementaryFilter {
  public:
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
   /// \brief Constructor
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   ComplementaryFilter();
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
   /// \brief Constructor witht initialization
   ///
   /// \param[in] dt Time step of the complementary filter
   /// \param[in] HighPass Initial value for the high pass filter
   /// \param[in] LowPass Initial value for the low pass filter
   ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   ComplementaryFilter(double dt, Vector3 HighPass, Vector3 LowPass);
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
   /// \brief Destructor
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   ~ComplementaryFilter() {}  // Empty destructor
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
   /// \brief Initialize
   ///
   /// \param[in] dt Time step of the complementary filter
   /// \param[in] HighPass Initial value for the high pass filter
   /// \param[in] LowPass Initial value for the low pass filter
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   void initialize(double dt, Vector3 HighPass, Vector3 LowPass);
 
-  ////////////////////////////////////////////////////////////////////////////////////////////////
-  ///
   /// \brief Compute the filtered output of the complementary filter
   ///
   /// \param[in] x Quantity handled by the filter
   /// \param[in] dx Derivative of the quantity
   /// \param[in] alpha Filtering coefficient between x and dx quantities
-  ///
-  ////////////////////////////////////////////////////////////////////////////////////////////////
   Vector3 compute(Vector3 const& x, Vector3 const& dx, Vector3 const& alpha);
 
-  Vector3 getX() { return x_; }    // Get the input quantity
-  Vector3 getDx() { return dx_; }  // Get the derivative of the input quantity
-  Vector3 getHighPass() {
-    return HighPass_;
-  }  // Get the high-passed internal quantity
-  Vector3 getLowPass() {
-    return LowPass_;
-  }  // Get the low-passed internal quantity
-  Vector3 getAlpha() {
-    return alpha_;
-  }  // Get the alpha coefficient of the filter
-  Vector3 getFilteredX() { return filteredX_; }  // Get the filtered output
+  /// Get the input
+  Vector3 getX() const { return x_; }
+  /// Get the derivative of the input
+  Vector3 getDx() const { return dx_; }
+  /// Get the high-passed internal quantity
+  Vector3 getHighPass() const { return HighPass_; }
+  /// Get the low-passed internal quantity
+  Vector3 getLowPass() const { return LowPass_; }
+  /// Get the alpha coefficient of the filter
+  Vector3 getAlpha() const { return alpha_; }
+  /// Get the filtered output
+  Vector3 getFilteredX() const { return filteredX_; }
 
  private:
   double dt_;          // Time step of the complementary filter
